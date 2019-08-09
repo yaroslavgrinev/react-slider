4858e8b59edb14822d87b3511aad62f8d5657671
yaroslavgrinev <yaroslav.grinev95@gmail.com>
2019-08-09 08:37:38 +0300

    adding backgound

---
+++ src/componets/app/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Slider from '../slider'
+
+const _App = () => {
+  return (
+    <div className='app'>
+      <Slider/>
+    </div>
+  )
+}
+
+export default _App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Slider from './componets/slider';
+import App from './componets/app';
 
-ReactDOM.render(<Slider />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
